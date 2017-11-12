525bd3b1d2cb531e96deddbc7ada72ef03f4192e
Jalell <jalell@surface.com>
2017-11-12 13:57:19 +0100

    initial commit

---
+++ app/src/main/java/de/davidgollasch/emiexercise1/FibonacciActivity.java
@@ -0,0 +1,64 @@
+package de.davidgollasch.emiexercise1;
+
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static de.davidgollasch.emiexercise1.R.id.buttonFire;
+import static de.davidgollasch.emiexercise1.R.id.textViewFibonacci;
+
+public class FibonacciActivity extends AppCompatActivity {
+
+    private TextView tvOutput;
+    private Button btnFire;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fibonacci);
+
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.tuAkzentfarbe1BlauHell)));
+
+        InitializeActivity();
+    }
+
+    private void InitializeActivity() {
+        tvOutput = (TextView) findViewById(textViewFibonacci);
+        btnFire = (Button) findViewById(buttonFire);
+
+        btnFire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Clear text view
+                tvOutput.setText("");
+
+                // Invoke calculation
+                CalculateFibonacciRow();
+            }
+        });
+    }
+
+    /**
+     * This method has to be filled...
+     */
+    private void CalculateFibonacciRow() {
+        String output = "";
+        int n = 0;
+        int fib1 = 1;
+        int fib2 = 1;
+        output = "1, 1";
+        for(int i = 0; i<28; i++){
+            n = fib1 + fib2;
+            fib1 = fib2;
+            fib2 = n;
+            output += ", " + n;
+        }
+        //output = "done" + n;
+        /* TODO: IMPLEMENT THIS */
+
+        tvOutput.setText(output);
+    }
+}
